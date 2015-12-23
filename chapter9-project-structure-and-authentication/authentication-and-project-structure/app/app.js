bc33a58d34ebfcb4f61a417e573deba6d6a92709
Kumar Abhinav <abhinav@Kumars-MacBook-Air.local>
2015-12-23 21:44:10 +0530

    chapter9-project-structure-and-authentication/

--- chapter9-project-structure-and-authentication/authentication-and-project-structure/app/app.js
+++ chapter9-project-structure-and-authentication/authentication-and-project-structure/app/app.js
@@ -2,13 +2,13 @@ angular
     .module('todoApp', ['ngRoute'])
     //Uncomment to start front end authentication
 
-    .run(function($rootScope, $location, AuthService) {
-        $rootScope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
-            if (!AuthService.getToken() && nextRoute.loginRequired) {
-                event.preventDefault();
-                $rootScope.$evalAsync(function() {
-                    $location.path('/login').search({message: 'Please login first!!!'});
-                });
-            }
-        });
-    });
+    // .run(function($rootScope, $location, AuthService) {
+    //     $rootScope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
+    //         if (!AuthService.getToken() && nextRoute.loginRequired) {
+    //             event.preventDefault();
+    //             $rootScope.$evalAsync(function() {
+    //                 $location.path('/login').search({message: 'Please login first!!!'});
+    //             });
+    //         }
+    //     });
+    // });
